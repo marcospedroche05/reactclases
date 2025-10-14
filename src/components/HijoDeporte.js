08c702b494bb0b22b132f3d2d6d84d5e6f01e92f
marcospedroche05 <marcospedroche62@gmail.com>
2025-10-14 14:22:43 +0200

    deporte avanzado

--- src/components/HijoDeporte.js
+++ src/components/HijoDeporte.js
@@ -4,7 +4,10 @@ class HijoDeporte extends Component {
   render() {
     return (
       <div>
-        <h1>Hijo deporte</h1>
+        <h2>{this.props.deporte}</h2>
+        <button onClick={this.props.favorito(this.props.deporte)}>
+          Mostrar favorito
+        </button>
       </div>
     );
   }
--- src/components/PadreDeportes.js
+++ src/components/PadreDeportes.js
@@ -1,10 +1,32 @@
 import { Component } from "react";
+import HijoDeporte from "./HijoDeporte";
 
 class PadreDeportes extends Component {
+  deportes = ["Futbol", "Baloncesto", "Natacion", "Tenis"];
+  state = {
+    favorito: "",
+  };
+  mostrarFavorito = (fav) => {
+    this.setState({
+      favorito: fav,
+    });
+  };
   render() {
     return (
       <div>
         <h1>Padre deportes</h1>
+        <h2 style={{ backgroundColor: "yellow" }}>
+          Mi deporte favorito es: {this.state.favorito}
+        </h2>
+        {this.deportes.map((deporte, index) => {
+          return (
+            <HijoDeporte
+              key={index}
+              deporte={deporte}
+              favorito={this.mostrarFavorito}
+            />
+          );
+        })}
       </div>
     );
   }
